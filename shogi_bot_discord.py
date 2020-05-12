d1cf2028cca0c75561ab9a1689d5fd5b96383bf5
Mori19 <55312081+Mori19@users.noreply.github.com>
2020-05-13 08:09:31 +1000

    Added Checkmate, scoring
    The game will now check for checkmate and has an endstate. Checkmate does not yet include the possibility of dropping a piece in the moves it checks to get out of checkmate. 
    The game keeps track of who has played games, and how many times you have won or lost.

--- shogi_bot_discord.py
+++ shogi_bot_discord.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import re
 import shogibot_token
 import shogi_pieces
+import pickle
 
 class Player:
     def __init__(self,who,hand,team,id):
@@ -11,21 +12,24 @@ class Player:
         self.hand = hand
         self.team = team
         self.threat = False
-        
+    def __repr__(self):
+        return "A player of a game of shogi"
+    def __str__(self):
+        return "A player of a game of shogi"      
 
-        
 class Game:
-    def __init__(self,ctx):
-        self.ctx = ctx
+    def __init__(self):
         self.playing = 1
         self.setup()
         self.occupy_positions()
         self.persistent_board = False
-
         
-    def setup(self):
-        to_send = ""
-        to_send += "Initialising\n"        
+    def __repr__(self):
+        return "A game of Shogi"
+    def __str__(self):
+        return "A game of Shogi"
+        
+    def setup(self): 
         self.turn = 'black'
         self.player1 = Player('vacant',[],'black','')
         self.player2 = Player('vacant',[],'white','')
@@ -88,9 +92,11 @@ class Game:
         if tuple(map(lambda a,b:a-b,new_position, position)) in piece.moves(self.occupied_positions) and False not in tuple(map(lambda a : a>=0, list(map(lambda a,b : b-a, new_position, (4,4))))):
             for other_piece in self.piece_list:
                 if other_piece.position == new_position and other_piece.team == piece.team and other_piece != piece:
+                    print("moving onto self")
                     return "A piece on your team is already there"
             return self.hypothesis_move_and_check_for_check(piece,new_position)
         else:
+            print("illegal move")
             return "Illegal move; it could be out of bounds or not in the piece movelist"
 
     def hypothesis_move_and_check_for_check(self,piece,new_position):
@@ -98,10 +104,6 @@ class Game:
         for save_piece in self.piece_list:
             save_state[save_piece] = [save_piece.position,save_piece.team,save_piece.isPromoted]
         save_hand = [self.player1.hand.copy(),self.player2.hand.copy()]
-        try: 
-            print(save_hand)
-        except:
-            print("She's cooked boss")
         piece.position = new_position
         print(self.check_take_piece(piece))
         self.check_for_threat()
@@ -164,6 +166,7 @@ class Game:
             return False
         else:
             return self.check_rules(piece,piece.position,new_pos)
+        
     def autopromotion_protocol(self,piece):
         if not piece.isPromoted and piece.position in piece.promotion_row(): 
             return self.promote(piece)
@@ -191,6 +194,7 @@ class Game:
                     self.player2.threat = True
                 elif piece.team == "white" and self.black_king.position == tuple(map(lambda a,b:a+b,piece.position,move)):
                     self.player1.threat = True
+                    
     def warn_check(self):
         to_send = ""
         if self.player1.threat: to_send+="Black is in check!"
@@ -209,10 +213,34 @@ class Game:
                 my_pieces.append(piece)
         my_active_piece = my_pieces[random.randint(0,len(my_pieces))]
           
+def check_for_checkmate(ctx,game):
+    save_state = pickle.dumps(game)
+    print(f'the game turn is {game.turn}')
+    for piece in game.piece_list:
+        if piece.team == game.turn and piece.position != (7,7):
+            print(f'the now piece is {piece.team}')
+            print(piece)
+            for move in piece.moves(game.occupied_positions):
+                print(f'the move to check is {move}')
+                if not game.enact_move(piece,tuple(map(lambda a,b:a+b,piece.position,move))):
+                    games[ctx.channel.id] = pickle.loads(save_state)
+                    print("pickle loaded")
+                    return False
+    game = pickle.loads(save_state)
+    return "Thats checkmate!"
 
+class Person:
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+        self.wins = 0
+        self.loss = 0
+        
+        
 
 bot = commands.Bot(command_prefix='!',description="A bot to play shogi with on discord. Currently lets you play minishogi.")
 games = {}
+people = {}
 
 @bot.event
 async def on_ready():
@@ -222,11 +250,12 @@ async def on_ready():
 
 @bot.command(name='new-game',help='make a game')
 async def initialise(ctx):
-    games[ctx.channel.id] = Game(ctx)
+    games[ctx.channel.id] = Game()
     await ctx.send(games[ctx.channel.id].draw_board())
 @bot.command(name='move',help='follow this command with a move')
 async def make_move(ctx,move):
     #global games
+    #print(games[ctx.channel.id].__dict__)
     print(games)
     if ctx.author.id != games[ctx.channel.id].player1.id and ctx.author.id != games[ctx.channel.id].player2.id:
         await ctx.send("You are not a player in the game!")
@@ -251,7 +280,7 @@ async def make_move(ctx,move):
                 if not games[ctx.channel.id].enact_move(active_piece, (horizontal,vertical)): 
                     message = games[ctx.channel.id].check_take_piece(active_piece)
                     if message: await ctx.send(message)
-                    if move[-1] == 'p': await ctx.send(game.promote(active_piece,last_pos))
+                    if move[-1] == 'p': await ctx.send(games[ctx.channel.id].promote(active_piece,last_pos))
                     games[ctx.channel.id].change_turn()
                 else: await ctx.send(games[ctx.channel.id].enact_move(active_piece, (horizontal,vertical)))
             else:
@@ -276,7 +305,19 @@ async def make_move(ctx,move):
         if not dropped: await ctx.send("You do not have a piece to drop/made an illegal drop")
     if games[ctx.channel.id].persistent_board: await ctx.send(games[ctx.channel.id].draw_board())
     games[ctx.channel.id].check_for_threat()
-    if games[ctx.channel.id].warn_check(): await ctx.send(games[ctx.channel.id].warn_check())
+    if games[ctx.channel.id].warn_check(): 
+        await ctx.send(games[ctx.channel.id].warn_check())
+        message = check_for_checkmate(ctx,games[ctx.channel.id])
+        if message: 
+            await ctx.send(message)
+            if games[ctx.channel.id].player1.id not in people.keys(): people[games[ctx.channel.id].player1.id] = Person(games[ctx.channel.id].player1.id,games[ctx.channel.id].player1.who)
+            if games[ctx.channel.id].player2.id not in people.keys(): people[games[ctx.channel.id].player2.id] = Person(games[ctx.channel.id].player2.id,games[ctx.channel.id].player2.who)
+            if games[ctx.channel.id].turn == "black":
+                people[games[ctx.channel.id].player1.id].loss +=1
+                people[games[ctx.channel.id].player2.id].wins +=1
+            else:
+                people[games[ctx.channel.id].player1.id].wins +=1
+                people[games[ctx.channel.id].player2.id].loss +=1
     
 @bot.command(name='show-board',help='show the board')
 async def draw_table(ctx):
@@ -299,6 +340,18 @@ async def register_player(ctx):
             return
     await ctx.send("The game is full")
     
+@bot.command(name='restore',help="this function is not yet implemented")
+async def restore_game(ctx):
+    await ctx.send("empty function")
+@bot.command(name='scoreboard',help="show the current leaderboard")
+async def show_scores(ctx):
+    to_send = "`SCOREBOARD:\n"
+    to_send += "{:<11}{:<5}{:<5}\n".format("Player","Wins","Loss")
+    for person in people.values():
+        to_send += '{:<15} {:>3} {:>3}\n'.format(person.name,person.wins,person.loss)
+    to_send+='`'
+    await ctx.send(to_send)
+
 @bot.command(name='rules',help='display the rules')
 async def get_help(ctx):
     await ctx.send("Moves are made in format piece, horizontal and then vertical (you can also specify where from if needed). eg white silver to column 3 row 2 would be s32.\nYou cannot drop a pawn in the same row as another pawn, nor can you drop it in the promotion row. The final row is the promotion row, and you can promote moving in or out.\nk - 王\ng - 金\ns - 銀  n - 成金\nb - 角  h ‐ 竜馬\nr - 車  d ‐ 竜王\nf ‐ 歩  t ‐ と金")
